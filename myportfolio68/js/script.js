be0f3c9538e577fcad26498d9c56e0821ce2d9e5
TOUKAM Sylvain <craig03@hotmail.fr>
2024-06-23 23:13:47 +0200

    Add files via upload

---
+++ myportfolio68/js/script.js
@@ -0,0 +1,8 @@
+/* ========================== Typing Animation =========================== */
+
+var typed = new Typed(".typing", {
+  strings: ["Developpeur Web", "Developpeur App Mobile", " Developpeur App Flutter"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
